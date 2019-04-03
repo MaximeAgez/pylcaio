0f1b127cfe24d5988744f66583333ff8adc3da7b
MaximeAgez <11.maxime.agez@gmail.com>
2019-04-03 15:38:43 -0400

    typo corrected in indexes

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,29 +15,40 @@ def random_LCA_system():
     productId3 = str(uuid.uuid4())
     productId4 = str(uuid.uuid4())
     productId5 = str(uuid.uuid4())
-    PRO_f = pd.DataFrame([['product1', productId1, 'activity1', 'activityNameId1', activityId1, 'CA-QC', 'CA', 1.1],
-                          ['product2', productId2, 'activity2', 'activityNameId2', activityId2, 'RER', 'RER', 1.2],
-                          ['product3', productId3, 'activity2', 'activityNameId2', activityId2, 'RoW', 'RoW', 1.3],
-                          ['product4', productId4, 'activity3', 'activityNameId3', activityId3, 'CH', 'CH', 1.4],
-                          ['product5', productId5, 'activity1', 'activityNameId1', activityId1, 'RoW', 'RoW', 1.5]],
-                         index=[str(productId1) + '_' + str(activityId1), str(productId2) + '_' + str(activityId2),
-                                str(productId3) + '_' + str(activityId2),
-                                str(productId4) + '_' + str(activityId3), str(productId5) + '_' + str(activityId1)],
+    PRO_f = pd.DataFrame([['product1', productId1, 'activity1', 'activityNameId1', activityId1, 'CA-QC', 'CA', 1.1,'sector1'],
+                          ['product2', productId2, 'activity2', 'activityNameId2', activityId2, 'RER', 'RER', 1.2,'sector2'],
+                          ['product3', productId3, 'activity2', 'activityNameId2', activityId2, 'RoW', 'RoW', 1.3,'sector3'],
+                          ['product4', productId4, 'activity3', 'activityNameId3', activityId3, 'CH', 'CH', 1.4,'sector3'],
+                          ['product5', productId5, 'activity1', 'activityNameId1', activityId1, 'RoW', 'RoW', 1.5,'sector5']],
+                         index=[str(activityId1) + '_' + str(productId1), str(activityId2) + '_' + str(productId2),
+                                str(activityId2) + '_' + str(productId3),
+                                str(activityId3) + '_' + str(productId4), str(activityId1) + '_' + str(productId5)],
                          columns=['productName', 'productId', 'activityName', 'activityNameId', 'activityId',
-                                  'geography', 'io_geography', 'price'])
+                                  'geography', 'io_geography', 'price', 'ProductTypeName'])
     A_ff = pd.DataFrame(np.random.random_sample((5, 5)),
-                        index=[str(productId1) + '_' + str(activityId1), str(productId2) + '_' + str(activityId2),
-                               str(productId3) + '_' + str(activityId2),
-                               str(productId4) + '_' + str(activityId3), str(productId5) + '_' + str(activityId1)],
-                        columns=[str(productId1) + '_' + str(activityId1), str(productId2) + '_' + str(activityId2),
-                                 str(productId3) + '_' + str(activityId3),
-                                 str(productId4) + '_' + str(activityId3),
-                                 str(productId5) + '_' + str(activityId1)])
+                        index=[str(activityId1) + '_' + str(productId1), str(activityId2) + '_' + str(productId2),
+                                str(activityId2) + '_' + str(productId3),
+                                str(activityId3) + '_' + str(productId4), str(activityId1) + '_' + str(productId5)],
+                        columns=[str(activityId1) + '_' + str(productId1), str(activityId2) + '_' + str(productId2),
+                                str(activityId2) + '_' + str(productId3),
+                                str(activityId3) + '_' + str(productId4), str(activityId1) + '_' + str(productId5)])
+    A_ff.iloc[0, 0] = 0
+    A_ff.iloc[2, 0] = 0
+    A_ff.iloc[3, 0] = 0
+    A_ff.iloc[1, 1] = 0
+    A_ff.iloc[3, 1] = 0
+    A_ff.iloc[2, 2] = 0
+    A_ff.iloc[0, 2] = 0
+    A_ff.iloc[3, 3] = 0
+    A_ff.iloc[1, 3] = 0
+    A_ff.iloc[4, 4] = 0
+    A_ff.iloc[0, 4] = 0
+    A_ff.iloc[3, 4] = 0
 
     F_f = pd.DataFrame(np.random.random_sample((3, 5)), index=['pollutant1', 'pollutant2', 'pollutant3'],
-                       columns=[str(productId1) + '_' + str(activityId1), str(productId2) + '_' + str(activityId2),
-                                str(productId3) + '_' + str(activityId2),
-                                str(productId4) + '_' + str(activityId3), str(productId5) + '_' + str(activityId1)])
+                       columns=[str(activityId1) + '_' + str(productId1), str(activityId2) + '_' + str(productId2),
+                                str(activityId2) + '_' + str(productId3),
+                                str(activityId3) + '_' + str(productId4), str(activityId1) + '_' + str(productId5)])
     C_f = pd.DataFrame(np.random.random_sample((3, 3)), index=['impact1', 'impact2', 'impact3'],
                        columns=['pollutant1', 'pollutant2', 'pollutant3'])
     STR_f = pd.DataFrame([['pollutant_name1', 'pollutant_unit1', 'pollutant_comp1', 'cas1'],
@@ -47,10 +58,10 @@ def random_LCA_system():
                          columns=['pollutant_name', 'pollutant_unit', 'pollutant_comp', 'cas'])
     y_f = pd.DataFrame(np.eye(len(A_ff)), A_ff.index, A_ff.columns)
 
-    list_to_hyb = [str(productId1) + '_' + str(activityId1),str(productId2) + '_' + str(activityId2),
-                   str(productId4) + '_' + str(activityId3)]
-    list_not_to_hyb = [str(productId3) + '_' + str(activityId2),str(productId5) + '_' + str(activityId1)]
-    listmarket = [str(productId3) + '_' + str(activityId2)]
+    list_to_hyb = [str(activityId1) + '_' + str(productId1),str(activityId2) + '_' + str(productId2),
+                   str(activityId3) + '_' + str(productId4)]
+    list_not_to_hyb = [str(activityId2) + '_' + str(productId3),str(activityId1) + '_' + str(productId5)]
+    listmarket = [str(activityId2) + '_' + str(productId3)]
 
     dict_dataframes = {'PRO_f': PRO_f, 'A_ff': A_ff, 'F_f': F_f, 'C_f': C_f, 'STR_f': STR_f, 'y_f': y_f,
                        'lca_database_name_and_version': lca_database_name_and_version, 'list_to_hyb':list_to_hyb,
@@ -95,9 +106,23 @@ def random_parameters():
     reference_year_IO = 2011
     number_of_countries_IO = 4
     number_of_products_IO = 5
+    number_of_RoW_IO = 0
+
+    io_categories = defaultdict()
+    io_categories['Agriculture'] = ['sector1','sector2']
+    io_categories['Electricity'] = ['sector3']
+    io_categories['Machinery'] = ['sector5']
+    io_categories['Services'] = ['sector4']
+    categories_same_functionality = ['Electricity']
+
+    STAM_table = pd.DataFrame([[1,1,1,1],[1,1,1,1],[1,0,1,1],[1,1,1,1]],index=io_categories.keys()
+                              ,columns=io_categories.keys())
 
     dict_parameters = {'listcountry': listcountry, 'listregions': listregions,
                        'countries_per_regions': countries_per_regions, 'replacements1': replacements1,
                        'reference_year_IO': reference_year_IO, 'number_of_countries_IO': number_of_countries_IO,
-                       'number_of_products_IO': number_of_products_IO}
+                       'number_of_products_IO': number_of_products_IO, 'number_of_RoW_IO': number_of_RoW_IO,
+                       'io_categories':io_categories,'STAM_table':STAM_table,
+                       'categories_same_functionality':categories_same_functionality}
     return dict_parameters
+
